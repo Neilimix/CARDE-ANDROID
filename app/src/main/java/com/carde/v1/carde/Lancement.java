ff32d4901db25e89aaa58803d73d4ba956e6fa7f
Neilimix <vermandel.maximilien@gmail.com>
2017-05-18 16:15:14 +0200

    Splash v1

--- app/src/main/java/com/carde/v1/carde/Lancement.java
+++ app/src/main/java/com/carde/v1/carde/Lancement.java
@@ -1,13 +1,26 @@
 package com.carde.v1.carde;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Lancement extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lancement);
+
+        new Handler().postDelayed(new Runnable() {
+
+            public void run() {
+                Intent i = new Intent(Lancement.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
