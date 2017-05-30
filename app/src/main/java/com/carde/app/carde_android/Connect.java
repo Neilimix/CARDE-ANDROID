101aca4ca995f0a00344388fcf3f683d4f9fdc2a
Neilimix <vermandel.maximilien@gmail.com>
2017-05-30 10:27:21 +0200

    30052017 1027

--- app/src/main/java/com/carde/app/carde_android/Connect.java
+++ app/src/main/java/com/carde/app/carde_android/Connect.java
@@ -1,9 +1,13 @@
 package com.carde.app.carde_android;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.ImageView;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -11,20 +15,55 @@ import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.facebook.login.widget.ProfilePictureView;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
-public class Connect extends AppCompatActivity {
+public class Connect extends AppCompatActivity{
+
+    ////////////////FB//////////////////
     private CallbackManager callbackManager;
     private TextView info;
     private LoginButton loginButton;
+    private ProfilePictureView profilePictureView;
+    private Button SignInLocal;
+    private TextView ignore;
+
+    ///////////////////////////
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ////////////////////FB///////////////
         FacebookSdk.sdkInitialize(getApplicationContext());
         callbackManager = CallbackManager.Factory.create();
         setContentView(R.layout.activity_connect);
         info = (TextView)findViewById(R.id.info);
+        profilePictureView = (ProfilePictureView) findViewById(R.id.friendProfilePicture);
         loginButton = (LoginButton)findViewById(R.id.login_button);
+        SignInLocal = (Button)findViewById(R.id.sign_in_button);
+        ignore=(TextView)findViewById(R.id.ignore);
+
+        ignore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ignore = new Intent(Connect.this, Default.class);
+                startActivity(ignore);
+            }
+        });
+
+        SignInLocal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent connect = new Intent(Connect.this, LoginActivity.class);
+                startActivity(connect);
+            }
+        });
 
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
@@ -32,6 +71,8 @@ public class Connect extends AppCompatActivity {
                 info.setText("User ID:  " +
                         loginResult.getAccessToken().getUserId() + "\n" +
                         "Auth Token: " + loginResult.getAccessToken().getToken());
+                profilePictureView.setProfileId(loginResult.getAccessToken().getUserId());
+
             }
 
             @Override
@@ -44,6 +85,7 @@ public class Connect extends AppCompatActivity {
                 info.setText("Login attempt failed.");
             }
         });
+        //////////////////////////////////////////
     }
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
